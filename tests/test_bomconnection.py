4c6e05acaa12f898264930660faf11a23329f5d1
Andrew Liu <mushywushy@gmail.com>
2020-11-23 05:36:04 +1100

    updated tests

--- tests/test_bomconnection.py
+++ tests/test_bomconnection.py
@@ -5,14 +5,31 @@ import requests
 
 logger = logging.getLogger(__name__)
 
+
 class TestBomConnection(unittest.TestCase):
     def test_verifySuccessfulBomConnection(self):
         """
             Test that a successful BOM connection can be made
             This test may unexpectedly fail if the BOM system is down
+
+            While we are here, check some basic elements within the response data
         """
         bom_response = requests.get(helpers.BOM_URL)
         self.assertEqual(bom_response.status_code, 200)
+        bom_json = bom_response.json()
+
+        # While we are here, check that we can see the station data
+        self.assertIn("observations", bom_json)
+        self.assertIn("data", bom_json["observations"])
+        stations = bom_json["observations"]["data"]
+
+        # A safe bet is to ensure that there is at LEAST one station
+        self.assertGreaterEqual(len(stations), 1)
+        station = stations[0]
+        self.assertIn("name", station)
+        self.assertIn("apparent_t", station)
+        self.assertIn("lat", station)
+        self.assertIn("lon", station)
 
     def test_verifyFailedBomConnection(self):
         """
